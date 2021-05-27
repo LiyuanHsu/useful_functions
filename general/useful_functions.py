fd25ebe6151378d0f4f0c6f2fe801231e5b44138
Liyuan Hsu <36778044+LiyuanHsu@users.noreply.github.com>
2021-05-27 02:41:36 +0200

    Update useful_functions.py
    add create_dir_and_save_img() function

--- general/useful_functions.py
+++ general/useful_functions.py
@@ -3,3 +3,12 @@ from datetime import datetime
 
 def print_time_to_miliseconds():
     print(datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f"))
+    
+    
+def create_dir_and_save_img(dir_name, image):
+    from datetime import datetime
+    import cv2
+    import os
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    cv2.imwrite(f"{dir_name}/{datetime.now().strftime('%Y-%m-%d-%H:%M:%S.%f')[:-3]}.jpg", image)
